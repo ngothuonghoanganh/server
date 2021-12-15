e9e4e5a74ba82332177ae23f11d7ce400610c8e9
khaphat128 <khaphat128@gmail.com>
2021-12-15 21:31:34 +0700

    done get all cate + update a cate

--- controllers/category.ts
+++ controllers/category.ts
@@ -26,5 +26,44 @@ class CategoriesController {
         }
     }
 
+    public getAllCate = async (req: any, res: any, next: any) => {
+        const {id} = req.user;
+        try {
+            const List = await Categories.query()
+                .select('categories.*')
+                .where('isdeleted', false)
+                .andWhere('userid', id);
+            res.send(List);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    public updateCate = async (req: any, res: any, next: any) => {
+        try {
+            const { categoryId } = req.params;
+            // const { id } = req.user;
+            
+            // console.log(req.user);
+            const {phone} = req.user;
+            console.log(phone)
+            let {
+                categoryName,
+                isDeleted = false
+            } = req.body;
+
+            await Categories.query()
+                .update({
+                    categoryname: categoryName,
+                    isdeleted: isDeleted,
+                })
+                .where("id", categoryId)
+                .andWhere("isdeleted", false);
+            return res.send('Category updated');
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
 }
 export const CateController = new CategoriesController();
