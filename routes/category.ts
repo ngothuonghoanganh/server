b9d8c43c1cf5d4030aa63b9026e10c286ba34bda
ngothuonghoanganh <ngothuonghoanganh1144@gmail.com>
2022-01-02 08:46:32 +0700

    update category

--- routes/category.ts
+++ routes/category.ts
@@ -1,29 +1,39 @@
 import * as express from "express";
+import { createValidator } from "express-joi-validation";
+
+// khởi tạo validator
+import authentication from "../controllers/authentication";
+import category from "../controllers/category";
+
+import { createBodySchema } from "../services/validation/category";
 // import { AuthenticationController } from "../controllers/authentication";
 // import { CateController } from "../controllers/category";
+const validator = createValidator();
 
 const router = express.Router();
 
-// router.post(
-//   "/",
-//   AuthenticationController.protected,
-//   AuthenticationController.checkRole(["Supplier"]),
-//   CateController.createNewCate
-// );
+router.post(
+  "/",
+  authentication.protected,
+  authentication.checkRole(["Supplier"]),
+  validator.body(createBodySchema),
+  category.createNewCate
+);
 
-// router.get(
-//   "/",
-//   AuthenticationController.protected,
-//   AuthenticationController.checkRole(["Supplier"]),
-//   CateController.getAllCate
-// );
+router.get(
+  "/",
+  authentication.protected,
+  authentication.checkRole(["Supplier"]),
+  category.getAllCate
+);
 
-// router.put(
-//   "/:categoryId",
-//   AuthenticationController.protected,
-//   AuthenticationController.checkRole(["Supplier"]),
-//   CateController.updateCate
-// );
+router.put(
+  "/:categoryId",
+  authentication.protected,
+  authentication.checkRole(["Supplier"]),
+  validator.body(createBodySchema),
+  category.updateCate
+);
 
 // // Categories mobile
 // router.get("/:userId", CateController.getAllCateMobi);
