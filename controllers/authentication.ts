541ea0fcb03b361268fbbeb335ee6f3b77faacdc
ngothuonghoanganh <ngothuonghoanganh1144@gmail.com>
2022-01-05 23:15:01 +0700

    update authentication

--- controllers/authentication.ts
+++ controllers/authentication.ts
@@ -19,13 +19,30 @@ class Authentication {
       const cookieOptions = {
         expiresIn: "24h",
       };
+      let info = {};
 
+      if (user.rolename === "Customer") {
+        info = await Customers.query()
+          .select()
+          .where("accountid", user.id)
+          .first();
+      } else if (user.rolename === "Supplier") {
+        info = await Suppliers.query()
+          .select()
+          .where("accountid", user.id)
+          .first();
+      } else {
+        info = await SystemProfile.query()
+          .select()
+          .where("accountid", user.id)
+          .first();
+      }
       res.cookie("jwt", token, cookieOptions);
 
       res.status(statusCode).json({
         status: "success",
         data: {
-          user: user,
+          user: { ...user, ...info },
           token: token,
         },
       });
@@ -218,7 +235,7 @@ class Authentication {
         .select()
         .where("rolename", roleName)
         .first();
-        console.log(roleName);
+      console.log(roleName);
 
       const newAccount = await Accounts.query().insert({
         username: username,
