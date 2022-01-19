d8f07f180eeea8e46967136ddfbecdfb76f48065
ngothuonghoanganh <ngothuonghoanganh1144@gmail.com>
2022-01-19 15:02:56 +0700

    update order

---
+++ models/orders.ts
@@ -0,0 +1,28 @@
+import { Model } from "objection";
+import * as connection from "./db/connection";
+
+Model.knex(connection.knex);
+
+export class Order extends Model {
+    static get tableName() {
+        return "orders";
+    }
+
+    id?: string;
+    customerid?: string;
+    productid?: string;
+    productname?: string;
+    quantity?: number;
+    iswholesale?: boolean
+    price?: number;
+    typeofproduct?: string;
+    customerdiscountid?: string;
+    status?: string;
+    campaignid?: string;
+    paymentid?: string;
+    discountprice?: number;
+    notes?: string;
+    shippingfee?: number;
+    createdat?: Date;
+    updatedat?: Date;
+}
