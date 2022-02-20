1bedbc38b0ea6f58e8371a0a20a29f869985d184
ngothuonghoanganh <ngothuonghoanganh1144@gmail.com>
2022-02-20 22:32:03 +0700

    update firebase

--- routes/index.ts
+++ routes/index.ts
@@ -1,4 +1,6 @@
 import * as express from "express";
+import { getDatabase, ref, set } from "firebase/database";
+import { database } from "../models/firebase/firebase";
 import { createValidator } from "express-joi-validation";
 import authentication from "../controllers/authentication";
 
@@ -11,13 +13,24 @@ const router = express.Router();
 
 router.get(
   "/",
-  authentication.protected,
-  authentication.checkRole(["Supplier","Customer"]),
+  // authentication.protected,
+  // authentication.checkRole(["Supplier", "Customer"]),
   //thêm validator được xác thực bơi schema vào middleware
-  validator.query(querySchema),
+  // validator.query(querySchema),
   async (req: any, res: any, next) => {
     try {
-      return res.status(200).send(req.user);
+      database.ref("users/8e77e9fa-7a91-4a0c-a83e-0525726bbdca").push({
+        helloword: "hellos",
+      });
+
+      // const data = await database.ref("users/8e77e9fa-7a91-4a0c-a83e-0525726bbdca").get()
+      // set(
+      //   ref(database as any, "users/" + "8e77e9fa-7a91-4a0c-a83e-0525726bbdca"),
+      //   {
+      //     username: "test1",
+      //   }
+      // );
+      return res.status(200).send("successful");
     } catch (error) {
       console.log(error);
     }
