a5f5de64f302fcd2b619613ea4939d5056db746f
khaphat128 <khaphat128@gmail.com>
2022-02-28 21:42:55 +0700

    add func of comment

--- controllers/comment.ts
+++ controllers/comment.ts
@@ -1,20 +1,21 @@
 import { Comments } from "../models/comment";
 
-class Comment{
-    public CreateNewComment = async(req: any, res: any, next: any)=>{
-        try{
+class Comment {
+    public CreateNewComment = async (req: any, res: any, next: any) => {
+        try {
             let {
-                orderId,
+                orderDetailId,
                 productId,
                 comment,
                 customerId,
                 rating
-                
-            }=req.body
 
-            const createNew= await Comments.query()
+            } = req.body
+            console.log(orderDetailId)
+
+            const createNew = await Comments.query()
                 .insert({
-                    orderid: orderId,
+                    oderdetailid: orderDetailId,
                     productid: productId,
                     comment: comment,
                     customerid: customerId,
@@ -25,54 +26,97 @@ class Comment{
                 message: 'successful',
                 data: createNew
             })
-        }catch(error){
+        } catch (error) {
             console.log(error)
         }
     };
 
-    public getCommentById = async(req: any, res: any, next: any)=>{
-        try{
+    public getCommentById = async (req: any, res: any, next: any) => {
+        try {
             const commentId = req.params.commentId;
-            const ListEntity =[
+            const ListEntity = [
                 'customers.avt',
                 'customers.firstname',
                 'customers.lastname'
             ]
             const data = await Comments.query()
-            .select('comments.*', ...ListEntity)
-            .join('customers', 'customers.id', 'comments.customerid')
-            .where('comments.id', commentId)
+                .select('comments.*', ...ListEntity)
+                .join('customers', 'customers.id', 'comments.customerid')
+                .where('comments.id', commentId)
 
             return res.status(200).send({
                 message: 'successful',
                 data: data
             })
-        }catch(error){
+        } catch (error) {
             console.log(error)
         }
     };
 
-    public getCommentByOrderId = async(req: any, res: any, next: any)=>{
-        try{
-            const orderId = req.query.orderId;
-            const ListEntity =[
+    public getCommentByOrderDetailId = async (req: any, res: any, next: any) => {
+        try {
+            const orderDetailId = req.query.orderDetailId;
+            const ListEntity = [
                 'customers.avt',
                 'customers.firstname',
                 'customers.lastname'
             ]
             const data = await Comments.query()
-            .select('comments.*', ...ListEntity)
-            .join('customers', 'customers.id', 'comments.customerid')
-            .where('orderid', orderId)
+                .select('comments.*', ...ListEntity)
+                .join('customers', 'customers.id', 'comments.customerid')
+                .where('oderdetailid', orderDetailId)
 
             return res.status(200).send({
                 message: 'successful',
                 data: data
             })
-        }catch(error){
+        } catch (error) {
             console.log(error)
         }
     };
+
+    public getCommentByProductId = async (req: any, res: any, next: any) => {
+        try {
+            const productId = req.body.productId;
+            const ListEntity = [
+                'customers.avt',
+                'customers.firstname',
+                'customers.lastname'
+            ]
+            const data = await Comments.query()
+                .select('comments.*', ...ListEntity)
+                .join('customers', 'customers.id', 'comments.customerid')
+                .where('comments.productid', productId)
+
+            return res.status(200).send({
+                message: 'successful',
+                data: data
+            })
+        } catch (error) {
+            console.log(error)
+        }
+    };
+
+    public countNumOfCommentByProductId = async (req: any, res: any, next: any) => {
+        try {
+            const productId = req.body.productId;
+            const data = await Comments.query()
+                .select(
+                    // 'comments.id as commentId',
+                    Comments.raw(`count(productid) as numOfComment`)
+                )
+                .where('productid', productId)
+                .groupBy('comments.id')
+            
+               const totalNumOfComment= Object.keys(data).length
+            return res.status(200).send({
+                message: 'successful',
+                data: ({totalNumOfComment})
+            })
+        } catch (error) {
+            console.log(error)
+        }
+    }
 }
 
 export default new Comment();
