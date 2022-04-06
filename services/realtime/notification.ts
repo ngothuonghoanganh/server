a68c3ffc4aa20d1f88b8e77555d5fe92281604d3
ngothuonghoanganh <ngothuonghoanganh1144@gmail.com>
2022-04-07 06:45:41 +0700

    update notif

--- services/realtime/notification.ts
+++ services/realtime/notification.ts
@@ -1,84 +1,77 @@
-
 import { Customers } from "../../models/customers";
 import { database } from "../../models/firebase/firebase";
 import { Notification } from "../../models/notification";
 import { Suppliers } from "../../models/suppliers";
 
 class Notif {
-    //send noti gom 3 thanh phan 
-    //userid
-    //message
-    //link
-    public sendNotiForWeb = async (data: any) => {
-        database.ref('notif/' + data.userid).set(data)
-        //insert data of noti to database
-        const insertNoti = await Notification.query()
-            .insert({
-                userid: data.userid,
-                link: data.link,
-                message: data.message,
-                status: "unread",
+  //send noti gom 3 thanh phan
+  //userid
+  //message
+  //link
+  public sendNotiForWeb = async (data: any) => {
+    database.ref("notif/" + data.userid).set(data);
+    //insert data of noti to database
+    const insertNoti = await Notification.query().insert({
+      userid: data.userid,
+      link: data.link,
+      message: data.message,
+      status: "unread",
+    });
+    // console.log(insertNoti)
+  };
 
-            })
-        // console.log(insertNoti)
+  public getNotifByAccountId = async (req: any, res: any, next: any) => {
+    try {
+      const accountId = req.query.accountId;
 
-    };
+      const data = await Notification.query()
+        .select()
+        .where("userid", accountId);
 
-    public getNotifByAccountId = async (req: any, res: any, next: any) => {
-        try {
-            const accountId = req.query.accountId;
-
-            const data = await Notification.query()
-                .select()
-                .where('userid', accountId)
-
-            return res.status(200).send({
-                message: 'successful',
-                data: data
-            })
-        } catch (error) {
-            console.log(error)
-        }
-    };
-
-    public getNotiForLoginUser = async (req: any, res: any) => {
-        try {
-            const userId = req.user.id;
-            let accountId;
-            accountId = await Customers.query().select('accountid').where('id', userId).first();
-            if (!accountId) {
-                accountId = await Suppliers.query().select('accountid').where('id', userId).first();
-
-            }
-
-            const data = await Notification.query().select().where('accountid', accountId);
-
-            return res.status(200).send({
-                message: 'successful',
-                data: data
-            })
-        } catch (error) {
-            console.log(error)
-        }
-    };
-
-    public updateNotifByListId = async (req: any, res: any) => {
-        try {
-            const notifs = req.body.notifs;
-
-            const data = await Notification.query().update({
-                status: "read"
-            })
-                .whereIn('id', notifs);
-
-            return res.status(200).send({
-                message: 'successful',
-                data: data
-            })
-        } catch (error) {
-            console.log(error)
-        }
+      return res.status(200).send({
+        message: "successful",
+        data: data,
+      });
+    } catch (error) {
+      console.log(error);
     }
+  };
+
+  public getNotiForLoginUser = async (req: any, res: any) => {
+    try {
+      const accountId = req.user.accountid;
+
+      const data = await Notification.query()
+        .select()
+        .where("userid", accountId);
+
+      return res.status(200).send({
+        message: "successful",
+        data: data,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  public updateNotifByListId = async (req: any, res: any) => {
+    try {
+      const notifs = req.body.notifs;
+
+      const data = await Notification.query()
+        .update({
+          status: "read",
+        })
+        .whereIn("id", notifs);
+
+      return res.status(200).send({
+        message: "successful",
+        data: data,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
 }
 
 export default new Notif();
