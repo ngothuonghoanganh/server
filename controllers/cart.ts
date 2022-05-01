40bca499031a59c4d0f3203c0d1d0669f461ae54
ngothuonghoanganh <ngothuonghoanganh1144@gmail.com>
2022-05-01 21:07:36 +0700

    fix cart

--- controllers/cart.ts
+++ controllers/cart.ts
@@ -1,20 +1,41 @@
 import { OrderDetail } from "../models/orderdetail";
 // import { Client } from "../models/redis/index";
+import { createClient } from "redis";
+import crypto from "crypto";
+import { Products } from "../models/products";
+import { Suppliers } from "../models/suppliers";
 
 class CartController {
+  client = createClient();
   public addToCart = async (req: any, res: any, next: any) => {
     try {
       const customerId = req.user.id; //customer id
+      let {
+        productId,
+        quantity,
+        inCampaign = false,
+        campaignId,
+        supplierId,
+      } = req.body;
 
-      let { productId, quantity, inCampaign = false, campaignId } = req.body;
-
-      const newCart: any = await OrderDetail.query().insert({
-        // customerid: customerId,
-        productId: productId,
+      const id = crypto.randomBytes(8).toString("hex") + `-${Date.now()}`;
+      const newCart = {
+        id: id,
+        productid: productId,
+        customerid: customerId,
+        campaignid: campaignId,
         quantity: quantity,
-        // incampaign: inCampaign,
-        // campaignid: campaignId,
-      });
+        inCampaign: inCampaign,
+        supplierid: supplierId,
+      };
+
+      await this.client.connect();
+      let data: any = (await this.client.get(`${req.user.id}`)) || "[]";
+      data = JSON.parse(data);
+      data.push({ ...newCart });
+      await this.client.set(`${req.user.id}`, JSON.stringify(data));
+
+      await this.client.QUIT();
 
       return res.status(200).send({
         message: "successful",
@@ -29,15 +50,21 @@ class CartController {
     try {
       const { cartId } = req.params;
       let { productId, quantity } = req.body;
-      const updateCart = await OrderDetail.query()
-        .update({
-          productId: productId,
-          quantity: quantity,
-        })
-        .where("id", cartId);
+      await this.client.connect();
+      let data: any = (await this.client.get(`${req.user.id}`)) || "[]";
+      // if (data) {
+      data = JSON.parse(data);
+      for (const element of data) {
+        if (element.id === cartId) {
+          element.quantity = quantity;
+          element.productid = productId;
+        }
+      }
+      await this.client.set(`${req.user.id}`, JSON.stringify(data));
+      await this.client.QUIT();
+
       return res.status(200).send({
         message: "cart updated",
-        data: updateCart,
       });
     } catch (error) {
       console.log(error);
@@ -47,14 +74,19 @@ class CartController {
   public deleteCart = async (req: any, res: any, next: any) => {
     try {
       const { cartId } = req.params;
-      // console.log(cartId)
-      const deleteCart: any = await OrderDetail.query()
-        .where("id", cartId)
-        .andWhere("orderdetail.ordercode", "is", null)
-        .del();
+      await this.client.connect();
+      let data: any = (await this.client.get(`${req.user.id}`)) || "[]";
+      // if (data) {
+      data = JSON.parse(data);
+      data.splice(
+        data.findIndex((item: any) => item.id === cartId),
+        1
+      );
+      await this.client.set(`${req.user.id}`, JSON.stringify(data));
+      await this.client.QUIT();
+
       return res.status(200).send({
         message: "cart deleted",
-        data: deleteCart,
       });
     } catch (error) {
       console.log(error);
@@ -64,33 +96,43 @@ class CartController {
   //suppliers.id = products.supplierid
   public getCartByUserId = async (req: any, res: any, next: any) => {
     try {
-      const { id } = req.user;
-      const listEntity = [
-        "products.id as productid",
-        "products.name as productname",
-        "products.retailprice as productretailprice",
-        "products.quantity as productquantity",
-        "products.description as productdescription",
-        "products.image as productimage",
-        "suppliers.id as supplierid",
-        "suppliers.name as suppliername",
-        "suppliers.email as supplieremai",
-        "suppliers.avt as supplieravt",
-        "suppliers.isdeleted as supplierisdeleted",
-        "suppliers.address as supplieraddress",
-      ];
-      // console.log(id)
-      const List = await OrderDetail.query()
-        .select("orderdetail.*", ...listEntity)
-        .join("products", "orderdetail.productid", "products.id")
-        .join("categories", "categories.id", "products.categoryid")
-        .join("suppliers", "suppliers.id", "categories.supplierid")
-        .where("orderdetail.customerid", id)
-        .andWhere("orderdetail.ordercode", "is", null);
+      await this.client.connect();
+      let data: any = (await this.client.get(`${req.user.id}`)) || "[]";
+      // if (data) {
+      data = JSON.parse(data);
+      for (const element of data) {
+        const [product, supplier] = await Promise.all([
+          Products.query()
+            .select(
+              "products.id as productid",
+              "products.name as productname",
+              "products.retailPrice as productretailprice",
+              "products.quantity as productquantity",
+              "products.description as productdescription",
+              "products.image as productimage"
+            )
+            .where("id", element.productid)
+            .first(),
+          Suppliers.query()
+            .select(
+              "suppliers.id as supplierid",
+              "suppliers.name as suppliername",
+              "suppliers.email as supplieremai",
+              "suppliers.avt as supplieravt",
+              "suppliers.isDeleted as supplierisdeleted",
+              "suppliers.address as supplieraddress"
+            )
+            .where("id", element.supplierid)
+            .first(),
+        ]);
 
+        Object.assign(element, { ...product, ...supplier });
+      }
+
+      await this.client.QUIT();
       res.status(200).send({
         message: "success",
-        data: List,
+        data: data,
       });
     } catch (error) {
       console.log(error);
